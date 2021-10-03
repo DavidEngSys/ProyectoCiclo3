a3241f3a8ef6613a329b10844d0e6f6e15e8ca1a
DavidEngSys <davidclf9005@gmail.com>
2021-10-03 17:48:21 -0500

    Create AlmacenRepository.java

---
+++ Backend/BackendInventario-1/src/main/java/com/inventarioTechnologyFinal/repository/AlmacenRepository.java
@@ -0,0 +1,9 @@
+package com.inventarioTechnologyFinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.inventarioTechnologyFinal.model.entity.Almacen;
+
+public interface AlmacenRepository extends JpaRepository<Almacen, Integer>{
+
+}
