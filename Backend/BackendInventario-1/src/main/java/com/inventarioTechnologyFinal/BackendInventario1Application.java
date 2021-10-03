4939c15e2a3c4156f6b8c3fdfe2fe728a2854890
DavidEngSys <davidclf9005@gmail.com>
2021-10-03 17:48:51 -0500

    Create BackendInventario1Application.java

---
+++ Backend/BackendInventario-1/src/main/java/com/inventarioTechnologyFinal/BackendInventario1Application.java
@@ -0,0 +1,13 @@
+package com.inventarioTechnologyFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInventario1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInventario1Application.class, args);
+	}
+
+}
