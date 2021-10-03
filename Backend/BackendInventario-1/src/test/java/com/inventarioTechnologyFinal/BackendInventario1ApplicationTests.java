3a2ceb43b19b6024a867ac1591d8f4e562ef5f9b
DavidEngSys <davidclf9005@gmail.com>
2021-10-03 17:48:29 -0500

    Create BackendInventario1ApplicationTests.java

---
+++ Backend/BackendInventario-1/src/test/java/com/inventarioTechnologyFinal/BackendInventario1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventarioTechnologyFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInventario1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
